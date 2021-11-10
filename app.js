6e465cc9b5b45f1054d777a7b2be9e622cdb3ba9
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-10 18:34:18 +0100

    Generate First colors

---
+++ app.js
@@ -0,0 +1,31 @@
+/*--------- DOM ELEMENTS ---------*/
+
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+/*--------- EVENTS ---------*/
+
+
+/*--------- FUNCTIONS ---------*/
+
+//Color Generator
+function generateHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        //Add the color to the background
+        div.style.backgroundColor = randomColor;
+        hexText.innerHTML = randomColor;
+    });
+}
+
+randomColors();
