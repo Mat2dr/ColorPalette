75d0a07e302872fd750e5bd66cc448359f6222d7
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-12 10:46:58 +0100

    Live slides color update

--- app.js
+++ app.js
@@ -99,6 +99,9 @@ function hslControls(e) {
         .set('hsl.h', hue.value);
 
     colorDivs[index].style.backgroundColor = color;
+
+    //Colorize inputs/sliders
+    colorizeSliders(color, hue,brightness,saturation);
 }
 
 function updateTextUI(index) {
