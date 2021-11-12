1878d3f5e35e8aaebc6144f4bfb91cc4616c10c5
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-12 11:42:42 +0100

    Open popup

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 const popup = document.querySelector('.copy-container');
+const adjustButton = document.querySelectorAll('.adjust');
+const closeAdjustments = document.querySelectorAll(".close-adjustment");
+const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors;
 
 /*--------- EVENTS ---------*/
@@ -26,6 +29,22 @@ popup.addEventListener('transitionend', () => {
     const popupBox = popup.children[0];
     popup.classList.remove('active');
     popupBox.classList.remove('active');
+});
+
+popup.addEventListener("transitionend", () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove("active");
+    popupBox.classList.remove("active");
+});
+adjustButton.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      openAdjustmentPanel(index);
+    });
+});
+closeAdjustments.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      closeAdjustmentPanel(index);
+    });
 });
 
 /*--------- FUNCTIONS ---------*/
@@ -160,6 +179,13 @@ function copyToClipboard(hex) {
     //Popup animation
     const popupBox = popup.children[0];
     popup.classList.add("active");
-}
+};
+
+function openAdjustmentPanel(index) {
+    sliderContainers[index].classList.toggle("active");
+  }
+  function closeAdjustmentPanel(index) {
+    sliderContainers[index].classList.remove("active");
+  }
 
 randomColors();
